2d8abc8ec903743042275c0736796d65944a5eeb
Uladzislau Hramovich <uladzislau_hramovich@epam.com>
2017-11-30 12:17:26 +0000

    remove useless lines

--- homework1/start.sh
+++ homework1/start.sh
@@ -1,7 +1,5 @@
 yum -y install gcc zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel tk-devel git patch
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-#echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
-#echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bashrc
