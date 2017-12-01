08ba442d5809b58f277b9186f6530166f1f4bfd4
uladzislau <vgrteam@gmail.com>
2017-12-01 15:25:19 +0300

    Update palindrome.py

--- homework2/palindrome.py
+++ homework2/palindrome.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 a = input("Enter your word: ")
-print(a[::-1])
-if a == a[::-1]:
-    print(a + " is a palindrome")
-else:
-    print(a + " is not a palindrome")
+
+
+def palindrome(a):
+    print(a[::-1])
+    if a == a[::-1]:
+        print(a + " is a palindrome")
+    else:
+        print(a + " is not a palindrome")
+
+palindrome(a)
