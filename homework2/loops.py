96ef84bb28fb96dda97c9ba0e0f4e97345665ed7
uladzislau <vgrteam@gmail.com>
2017-12-01 15:24:05 +0300

    Update loops.py

--- homework2/loops.py
+++ homework2/loops.py
@@ -1,12 +1,12 @@
-while True:
-    N = int(input("Enter an integer. (Enter 0 if you want to exit) : "))
+N = int(input("Enter your number: "))
+def loops(N):
     base = 0
     if N > 20:
         print("Your integer is greater than 20! Enter an integer which is not greater than 20 ")
-        continue
-    elif N == 0:
-        break
     else:
         while base < N:
-            print(base*base)
+            print(base * base)
             base += 1
+        return
+
+loops(N)
