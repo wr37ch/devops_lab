e925d1b731344f89e59557bf3b9bcdf09532c022
uladzislau <vgrteam@gmail.com>
2017-12-01 18:47:06 +0300

    Update StringFormatting.py

--- homework2/StringFormatting.py
+++ homework2/StringFormatting.py
@@ -3,15 +3,12 @@ N = int(input("Enter an integer: "))
 
 def StringFormatting(N):
     base = 1
-    if N > 99:
-        print("Your integer is greater than 99! Enter an integer which is less  than 99 ")
-    elif N < 1:
-        print("Your integer is less than 1! Enter an integer which is  greater than 1 ")
-    else:
+    if 99 >= N >= 1:
         while base <= N:
             width = len('{0:b}'.format(N))
             print('{0:{w}d}'.format(base, w=width), '{0:{w}o}'.format(base, w=width),
                   '{0:{w}x}'.format(base, w=width).upper(), '{0:{w}b}'.format(base, w=width), )
             base += 1
-
+    else:
+        print("Your integer doesn't match constraints")
 StringFormatting(N)
