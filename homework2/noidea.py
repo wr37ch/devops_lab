c8d1b149ff345911b2364561c37342a840c4333f
uladzislau <vgrteam@gmail.com>
2017-12-05 22:31:52 +0300

    Update noidea.py

--- homework2/noidea.py
+++ homework2/noidea.py
@@ -1,10 +1,13 @@
-ARRAY = {int(x) for x in input("ARRAY NUMBERS: ").split()}
-A = {int(x) for x in input("A SET NUMBERS: ").split()}
-B = {int(x) for x in input("B SET NUMBERS: ").split()}
+n_m = {int(x) for x in input().split()}
+ARRAY = {int(x) for x in input().split()}
+A = {int(x) for x in input().split()}
+B = {int(x) for x in input().split()}
 happiness_counter = 0
 for x in ARRAY:
-    if x in A:
+    if x in A and x not in B:
         happiness_counter += 1
-    elif x in B:
+    elif x in B and x not in A:
         happiness_counter -= 1
+    else:
+        continue
 print(happiness_counter)
